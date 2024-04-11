9fa906a167f355bf9910aa467af9136cdd69ac7d
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-04-11 19:57:52 -0400

    started homepage comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,7 +35,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Navbar />
+      
     </ApolloProvider>
   );
 }
---
+++ client/src/components/Homepage/index.jsx
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+
+const styles = {
+    homePageStyle: {
+
+    },
+};
+
+export function HomePage() {
+    return (
+      <div style={styles.homePageStyle} className="homePage">
+        <img src="" alt=""></img> 
+        <p> Hello world</p>
+       
+      </div>
+    );
+  }
+  
+  export default HomePage;
