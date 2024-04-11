d9c8f3d16cd5e82ab91127766bc226f25b14d2b0
kathylopez97 <146994593+kathylopez97@users.noreply.github.com>
2024-04-10 19:41:09 -0700

    deploying indexhtml

--- server/server.js
+++ server/server.js
@@ -19,13 +19,17 @@ const startApolloServer = async () => {
     await server.start();
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+ 
   
     app.use('/graphql', expressMiddleware(server));
-  
-      app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-      });
+  if (process.env.NODE_ENV === "production" ) {  
+    app.use(express.static(path.join(__dirname, '../client/dist'))); 
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+
+  } 
+    
   
     db.once('open', () => {
       app.listen(PORT, () => {
