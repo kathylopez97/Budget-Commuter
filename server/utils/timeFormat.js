93d3ec91b73609e94c89cd75ac1cdf0896049d7d
Daniel Jutila <daniel.jutila@gmail.com>
2024-04-10 19:33:00 -0500

    Coded SignupForm and LoginForm

--- server/utils/timeFormat.js
+++ server/utils/timeFormat.js
@@ -3,6 +3,7 @@ const moment = require('moment');
 //From https://momentjs.com/
 //using moment package to format the date
 const timeFormat = (date) => {
+    //'Could be MMM Do YYYY'
     return moment(date).format('MMMM Do YYYY, h:mm:ss a');
 };
 module.exports = timeFormat;
