72d41d8a6d07a417aae8b7130f8e8ead27f1903f
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-04-11 20:43:50 -0400

    added hompage signup and login form

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-// import SignUpForm from '../SignupForm';
-// import LoginForm from '../LoginForm';
+import SignUpForm from '../SignupForm';
+import LoginForm from '../LoginForm';
 
 import Auth from '../../utils/auth';
+
 
 const AppNavbar = () => {
   // set modal display state
