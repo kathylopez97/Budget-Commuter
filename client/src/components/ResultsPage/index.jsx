5434a1dc508f015c7fcdab9a24a6af660bd412e4
Jason <JasonArriaza10@gnail.com>
2024-04-15 20:45:49 -0500

    made a rough mock up of the results page after running a search

---
+++ client/src/components/ResultsPage/index.jsx
@@ -0,0 +1,15 @@
+// ResultsPage.jsx
+import React from 'react';
+import ResultsSelection from '../ResultSelection';
+
+const ResultsPage = () => {
+    return (
+        <div>
+            <h1>Search Results</h1>
+            <ResultsSelection />
+        </div>
+    );
+};
+
+export default ResultsPage;
+
